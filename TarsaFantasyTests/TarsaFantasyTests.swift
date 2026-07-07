e199a0f4eb33490a27895b631bfd0ba9895bb78c
Jordan R <jordanar189@gmail.com>
2026-07-07 11:44:21 -0400

    Add TarsaFantasyTests unit test target

---
+++ TarsaFantasyTests/TarsaFantasyTests.swift
@@ -0,0 +1,18 @@
+//
+//  TarsaFantasyTests.swift
+//  TarsaFantasyTests
+//
+//  Created by Jordan Ramsey on 7/7/26.
+//
+
+import Testing
+
+struct TarsaFantasyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
